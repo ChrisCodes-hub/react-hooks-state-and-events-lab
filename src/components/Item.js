cff1e84b974dda5c34edca4ce7d4ae864b94a560
christopherchege <chrisshege35@gmail.com>
2025-04-16 22:39:32 +0300

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React,{useState}from "react";
 
 function Item({ name, category }) {
+   const [isAdded, setIsAdded] = useState(false);
+  const toggleBtn = (e) => { 
+    setIsAdded(!isAdded);
+   }
   return (
-    <li className="">
+    <li className={isAdded ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+         onClick={toggleBtn} 
+         className={isAdded?'remove':'add'}>
+         {isAdded ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
